4bcdb468c154b576d9e53db72989544242eb8a5a
Pascal <74536354+Stylex78@users.noreply.github.com>
2022-09-05 17:18:32 +0200

    Add files via upload

--- LED_roulette.ino
+++ LED_roulette.ino
@@ -1,46 +1,24 @@
-/*
-  Blink
-
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Blink
-*/
 
 //Vars
 
-int pRED = 12;
-int pYELLOW = 11;
-int pGREEN = 10;
-int pBLUE = 9;
+int pARRAY [8] = {5,6,7,8,9,10,11,12};
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  pinMode(pRED, OUTPUT);
-  pinMode(pYELLOW, OUTPUT);
-  pinMode(pGREEN, OUTPUT);
-  pinMode(pBLUE, OUTPUT);
+  //Loop through array
+  for(int i = 0; i < sizeof(pARRAY)/sizeof(*pARRAY); i++) {
+    //set each to pinmode output
+    pinMode(i, OUTPUT);
+  }
 }
 
 // the loop function runs over and over again forever
 
-int pCURRENT = 12; //12 rood, 11 geel, 10 groen, 9 blauw.
+int pCURRENT = 12; //12 rood, 11 geel, 10 groen, 9 blauw, etc.
+int pNEXT = 11;
 int loopState = 1; //1 = --, 2 = ++
+int loopStateNEXT = 1;
+
 void loop() {
   if(loopState == 1) {
     pCURRENT-- ;
@@ -48,15 +26,47 @@ void loop() {
     pCURRENT++ ;
   }
   
-  if(pCURRENT <= 9) {
+  if(pCURRENT <= 5) {
     loopState = 2;
   }
   if(pCURRENT >= 12) {
     loopState = 1;
   }
+
+  if(loopState == 1) {
+    pNEXT-- ;
+  } else if ( loopState == 2) {
+    pNEXT++ ;
+  }
   
+  if(pCURRENT <= 5) {
+    loopStateNEXT = 2;
+  }
+  if(pCURRENT >= 12) {
+    loopStateNEXT = 1;
+  }
+
+  /*
   digitalWrite(pCURRENT, HIGH);
   delay(100);
   digitalWrite(pCURRENT, LOW);
-  delay(50);
+  delay(20);
+  */
+
+  /*
+
+  // Fade in
+  for(int ledVal = 0; ledVal <= 255; ledVal +=1) {
+    analogWrite(pCURRENT, ledVal);
+      delay(1);
+  }  
+  */
+int ledValInverse = 0;
+  // Fade uit
+  for(int ledVal = 255; ledVal >= 0; ledVal -=1) {
+    analogWrite(pCURRENT, ledVal);
+    ledValInverse = 255 - ledVal;
+    analogWrite(pNEXT, ledValInverse);
+    delay(1);
+  }
 }
